a609784cf9461b733e7a9b8af97d458a89fecb14
mohammad-abdulkhaliq <mohammad28march1993@gmail.com>
2013-05-07 00:57:52 +0200

    SRM 145 DIV 1 Code Fixes

--- topcoder/Bonuses.java
+++ topcoder/Bonuses.java
@@ -2,6 +2,8 @@
 import java.util.Arrays;
 
 public class Bonuses {
+	
+	static int last = 0;
 
 	public static int[] getDivision(int[] points) { 
 
@@ -12,19 +14,19 @@ public class Bonuses {
 		for(int i : points)
 			total+=i;
 		for(int i = 0; i < points.length; i++) { 
-			tmp = (points[i]/total)*100;
+			tmp = ((double)points[i]/total)*100;
 			left_over += tmp - Math.floor(tmp);
 			percent[i] = (int)Math.floor(tmp);
 		}
-		//Arrays.sort(points);
-		int bonus = (int) ((left_over/total) * 100);
+		int bonus = (int) Math.ceil(left_over);
 		int [] tmp_points = copyArr(points);
 		Arrays.sort(tmp_points);
-		for(int i = tmp_points.length - 1; i >=0;  i++){
-			int key = find(percent, tmp_points[i]);
+		for(int i = tmp_points.length - 1; i >= 0; i--){
+			int key = find(points, tmp_points[i]);
+			
 			if (bonus != 0){
-				percent[key]+=1;
-				bonus --;
+				percent[key]++;
+				bonus--;
 			}
 			else break;
 		}
@@ -32,16 +34,23 @@ public class Bonuses {
 	}
 
 	public static void main (String [] args) { 
-
-		double x = 1.32;
-		System.out.println((int)Math.floor(x));
+	
+		int [] x = getDivision(new int[]{5,5,5,5,5,5});
+		System.out.println(x[0] + " " + x[1] + " " + x[2]);
 	}
 
-	public static int find(int [] src, int key) { 
+	public static int find(int [] src, int value) { 
 
-		for(int i = 0; i < src.length; i++)
-			if(key == src[i])
+		for(int i = 0; i < src.length; i++) {
+			if(value == src[last]) {
+				last++;
+				return last-1;
+			}
+			if(value == src[i]) {
 				return i;
+			}
+			
+		}
 		return -1;
 	}
 	public static int [] copyArr(int [] src) { 
@@ -51,5 +60,6 @@ public class Bonuses {
 			dest[i] = src[i];
 		return dest;
 	}
+	
 
 }
